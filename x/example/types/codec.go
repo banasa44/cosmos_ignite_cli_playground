64391f5cfb80401d82a78ed0b53e5edeebdce0bb
banasa44 <cfbennassar@gmail.com>
2025-02-03 15:59:59 +0100

    delete post

--- x/example/types/codec.go
+++ x/example/types/codec.go
@@ -14,6 +14,9 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgCreatePost{},
 	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgDeletePost{},
+	)
 	// this line is used by starport scaffolding # 3
 
 	registry.RegisterImplementations((*sdk.Msg)(nil),
