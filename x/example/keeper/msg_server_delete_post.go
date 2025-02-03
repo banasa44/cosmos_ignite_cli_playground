fe0a9219e791599b02336bad619c1de965c24d85
banasa44 <cfbennassar@gmail.com>
2025-02-03 16:39:52 +0100

    query posts

--- x/example/keeper/msg_server_delete_post.go
+++ x/example/keeper/msg_server_delete_post.go
@@ -12,14 +12,14 @@ import (
 )
 
 func (k msgServer) DeletePost(goCtx context.Context, msg *types.MsgDeletePost) (*types.MsgDeletePostResponse, error) {
-    ctx := sdk.UnwrapSDKContext(goCtx)
-    val, found := k.GetPost(ctx, msg.Id)
-    if !found {
-        return nil, errorsmod.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
-    }
-    if msg.Creator != val.Creator {
-        return nil, errorsmod.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
-    }
-    k.RemovePost(ctx, msg.Id)
-    return &types.MsgDeletePostResponse{}, nil
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	val, found := k.GetPost(ctx, msg.Id)
+	if !found {
+		return nil, errorsmod.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+	if msg.Creator != val.Creator {
+		return nil, errorsmod.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+	k.RemovePost(ctx, msg.Id)
+	return &types.MsgDeletePostResponse{}, nil
 }
